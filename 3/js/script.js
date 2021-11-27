5aa9d2ab0c2c3899bab4612a432af556a194dfb8
Gerasimoivhc Sergey <wmmoee@gmail.com>
2021-11-27 12:22:13 +0300

    feat: added fetch

--- 3/js/script.js
+++ 3/js/script.js
@@ -240,10 +240,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //для json заголовок нужен
+            // request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //для json заголовок нужен
             const formData = new FormData(form);
 
             const dataObject = {};
@@ -251,20 +248,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 dataObject[key] = item;
             });
 
-            const jsonData = JSON.stringify(dataObject);
-
-            request.send(jsonData);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            })
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                }, //эта часть не нужна, так как отправляем не JSON, а formData и нужна если наоборот
+                body: JSON.stringify(dataObject)
+            }).then(data => data.text()
+            ).then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
         });
 
         function showThanksModal(message) {
